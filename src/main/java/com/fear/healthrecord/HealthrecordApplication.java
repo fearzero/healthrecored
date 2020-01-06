efb5d19702dd9888734c4036d4575ed6a1a41d58
赵连铜 <zhaoliantonng@bonc.com.cn>
2020-01-06 14:27:38 +0800

    环境建立

---
+++ src/main/java/com/fear/healthrecord/HealthrecordApplication.java
@@ -0,0 +1,13 @@
+package com.fear.healthrecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthrecordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthrecordApplication.class, args);
+    }
+
+}
