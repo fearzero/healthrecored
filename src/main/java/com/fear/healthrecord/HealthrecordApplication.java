87d625722fe71e4ab353a5a13f9dcc27871827ac
赵连铜-东方国信-接触中心 <zhaoliantong@bonc.com.cn>
2020-04-24 15:05:52 +0800

    功能跟新修改

--- src/main/java/com/fear/healthrecord/HealthrecordApplication.java
+++ src/main/java/com/fear/healthrecord/HealthrecordApplication.java
@@ -3,17 +3,22 @@ package com.fear.healthrecord;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @ServletComponentScan(basePackages = { "com.fear.healthrecord" })
 @MapperScan("com.fear.healthrecord.mapper")
 
 
-public class HealthrecordApplication {
+public class HealthrecordApplication  extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(HealthrecordApplication.class, args);
     }
-
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(HealthrecordApplication.class);
+    }
 }
